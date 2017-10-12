147e19ab8db21b5fe0441a83b996e0e44e3bf65f
ignaszienius <30698624+ignaszienius@users.noreply.github.com>
2017-10-12 18:19:15 +0300

    veikia tadaaaam

--- importData.php
+++ importData.php
@@ -3,40 +3,6 @@
 include 'dbConfig.php';
 
 if(isset($_POST['importSubmit'])) {
-                   /* 
-                    switch ($array1[0]) {
-                        case "color_map":
-                        switch ($array2[0]) { 
-                            case 'white':
-                                $db->query("UPDATE suppressed SET correct_value = 'white'");
-                                break;
-                            case 'grey':
-                                $db->query("UPDATE suppressed SET correct_value = 'grey'");
-                                break;
-                            case 'black':
-                                $db->query("UPDATE suppressed SET correct_value = 'black'");
-                                break;
-                            case 'red':
-                                $db->query("UPDATE suppressed SET correct_value = 'red'");
-                                break;
-                            case 'yellow':
-                                $db->query("UPDATE suppressed SET correct_value = 'yellow'");
-                                break;
-                            case 'pink':
-                                $db->query("UPDATE suppressed SET correct_value = 'pink'");
-                                break;
-                            case 'dark grey':
-                                $db->query("UPDATE suppressed SET correct_value = 'dark grey'");
-                                break;
-                            case 'navy blue':
-                                $db->query("UPDATE suppressed SET correct_value = 'navy blue'");
-                                break;
-                            case 'purple':
-                                $db->query("UPDATE suppressed SET correct_value = 'purple'");
-                                break;       
-                            default:
-                                break;}
-                                */
                            
 	//validate whether uploaded file is a csv file
 	$csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
@@ -44,32 +10,29 @@ if(isset($_POST['importSubmit'])) {
 		if (is_uploaded_file($_FILES['file']['tmp_name'])) {
 			//deletes all records from db table
 			$db->query("TRUNCATE suppressed");
+
 			//open uploaded csv file with read only mode
 			$csvFile = fopen($_FILES['file']['tmp_name'], 'r');
-
 
 			//skip first line
 			fgetcsv($csvFile);
 
-
 			//parse data from csv file line by line
 			while (($line = fgetcsv($csvFile)) !== FALSE) {
-				//insert data into database
-				$db->query("INSERT INTO suppressed (SKU, name, ASIN, alert_name, alert_type, field_name, internal_name, correct_value, explanation, date_value) VALUES ('" . $line[0] . "','" . $line[1] . "','" . $line[2] . "','" . $line[3] . "','" . $line[4] . "','" . $line[5] . "','" . $line[6] . "','" . explode('-', $line[0])[1] . "','" . $line[7] . "','" . $line[8] . "')");
-			}
-
+			//insert data into database
+			$db->query("INSERT INTO suppressed (SKU, name, ASIN, alert_name, alert_type, field_name, internal_name, correct_value, explanation, date_value) VALUES ('" . $line[0] . "','" . $line[1] . "','" . $line[2] . "','" . $line[3] . "','" . $line[4] . "','" . $line[5] . "','" . $line[6] . "','" . explode('-', $line[0])[1] . "','" . $line[8] . "','" . $line[9] . "')");
+			} 
 			//close opened csv file
 			fclose($csvFile);
 
 			$status = 'succ';
-		  } else {
+		  	}
+		else {
 			$status = 'err';
 		   }
-	       } else {
-		$status = 'invalid_file';
-	     
-          }
-    
+	} else {
+		$status = 'invalid_file';  
+    }  
 }
 //redirect to the listing page
 header("Location: index.php?status=".$status);
--- index.php
+++ index.php
@@ -22,6 +22,8 @@ if(!empty($_GET['status'])){
     }
 }
 ?>
+<!DOCTYPE html>
+<html>
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
@@ -29,6 +31,7 @@ if(!empty($_GET['status'])){
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </head>
+<body>
 <div class="container-fluid">
     <?php if(!empty($statusMsg)){
         echo '<div class="alert '.$statusMsgClass.'">'.$statusMsg.'</div>';
@@ -76,10 +79,12 @@ if(!empty($_GET['status'])){
                       <td><?php echo $row['date_value']; ?></td>
                     </tr>
                     <?php } }else{ ?>
-                    <tr><td colspan="14">No records found...</td></tr>
+                    <tr><td colspan="10">No records found...</td></tr>
                     <?php } ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+</body>
+</html>
